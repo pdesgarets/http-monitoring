b34eb2f127b754da5ebddc9eca28f8d3486f5aed
Paul des Garets <paul.des-garets@student.ecp.fr>
2015-11-21 17:57:36 +0100

    quick and basic beginning

---
+++ README.md
@@ -0,0 +1,14 @@
+Simple HTTP Monitoring
+======================
+
+This script aims at monitoring W3C complient HTTP logs and show miscellaneous informations about what's going on.
+
+To test it, download, `npm install` then `npm start` (of course you'll need node & npm).
+
+TODO :
+
+- decide which stats could be interesting
+- fetch and display those stats
+- tests
+- maybe start again from scratch, depending on the perfs and the correctness.
+
---
+++ main.js
@@ -0,0 +1,51 @@
+'use strict';
+var Tail = require('tail').Tail;
+var events = require('events');
+var emitter = new events.EventEmitter();
+var tail = new Tail('/tmp/test.log');
+var topRanks = {};
+var sections = [];
+var nbReq = 0;
+var triggered = false;
+var threshold = 50;
+
+tail.on('line', function (data) {
+        var section = /"\w+\s\/(\w*).*"/.exec(data)[1];
+        if (!section) {
+                // We replace root with a forbidden character in URL
+                section = "^";
+        }
+        if (Object.keys(topRanks).indexOf(section) > -1) {
+                topRanks[section] = topRanks[section] +1;
+        } else {
+                topRanks[section] = 1;
+        }
+        if (sections.indexOf(section) === -1) {
+                sections.push(section);
+        }
+        nbReq = nbReq + 1;
+        emitter.emit('nbReqUpdated');
+        setTimeout(function() {
+                emitter.emit('nbReqUpdated');
+                nbReq = nbReq -1;
+        }, 120000);
+});
+
+setInterval(function () {
+    console.log(new Date(), sections.sort(function(a,b) {
+            return topRanks[b] - topRanks[a];
+    }));
+}, 10000);
+
+emitter.on('nbReqUpdated', function() {
+        if (nbReq > threshold && !triggered) {
+                triggered = true;
+                console.log('oups ! it triggered with ', nbReq, ' at ', new Date());
+        } else if (nbReq < threshold) {
+                if (triggered) {
+                        triggered = false;
+                        console.log('Yeah ! it recovered with ', nbReq, ' at ', new Date());
+                }
+        }
+});
+
