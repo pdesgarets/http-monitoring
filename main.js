b2767f0849f92145a53b1bd44e5f53f093e6ab6b
Paul des Garets <paul.des-garets@student.ecp.fr>
2015-12-01 01:12:34 +0100

    test skeleton

--- main.js
+++ main.js
@@ -1,56 +1,65 @@
+/*jslint node: true */
 'use strict';
 var Tail = require('tail').Tail;
 var events = require('events');
 var emitter = new events.EventEmitter();
-var tail = new Tail('/tmp/test.log');
+var tail;
 var topRanks = {};
 var sections = [];
 var nbReq = 0;
 var triggered = false;
 var threshold = 50;
 
-tail.on('line', function (data) {
-        var matched = /"\w+\s\/([\w$_.-]*).*"/.exec(data);
-        if (matched == null) {
-                // For instance : OPTIONS * will not match.
-                return;
-        }
-        section = matched[1];
-        if (!section) {
-                // We replace root with a forbidden character in URL
-                section = "^";
-        }
-        if (Object.keys(topRanks).indexOf(section) > -1) {
-                topRanks[section] = topRanks[section] +1;
-        } else {
-                topRanks[section] = 1;
-        }
-        if (sections.indexOf(section) === -1) {
-                sections.push(section);
-        }
-        nbReq = nbReq + 1;
-        emitter.emit('nbReqUpdated');
-        setTimeout(function() {
-                emitter.emit('nbReqUpdated');
-                nbReq = nbReq -1;
-        }, 120000);
-});
+if (process.env.NODE_ENV != 'test') {
+  bootstrap('/tmp/test.log');
+}
 
-setInterval(function () {
+function bootstrap(file) {
+  tail = new Tail(file);
+  tail.on('line', function (data) {
+    var matched = /"\w+\s\/([\w$_.-]*).*"/.exec(data);
+    if (matched === null) {
+      // For instance : OPTIONS * will not match.
+      return;
+    }
+    var section = matched[1];
+    if (!section) {
+      // We replace root with a forbidden character in URL
+      section = '^';
+    }
+    if (Object.keys(topRanks).indexOf(section) > -1) {
+      topRanks[section] = topRanks[section] +1;
+    } else {
+      topRanks[section] = 1;
+    }
+    if (sections.indexOf(section) === -1) {
+      sections.push(section);
+    }
+    nbReq = nbReq + 1;
+    emitter.emit('nbReqUpdated');
+    setTimeout(function() {
+      emitter.emit('nbReqUpdated');
+      nbReq = nbReq -1;
+    }, 120000);
+  });
+
+  setInterval(function () {
     console.log(new Date(), sections.sort(function(a,b) {
-            return topRanks[b] - topRanks[a];
+      return topRanks[b] - topRanks[a];
     }));
-}, 10000);
+  }, 10000);
 
-emitter.on('nbReqUpdated', function() {
-        if (nbReq > threshold && !triggered) {
-                triggered = true;
-                console.log('oups ! it triggered with ', nbReq, ' at ', new Date());
-        } else if (nbReq < threshold) {
-                if (triggered) {
-                        triggered = false;
-                        console.log('Yeah ! it recovered with ', nbReq, ' at ', new Date());
-                }
-        }
-});
+  emitter.on('nbReqUpdated', function() {
+    if (nbReq > threshold && !triggered) {
+      triggered = true;
+      console.log('oups ! it triggered with ', nbReq, ' at ', new Date());
+    } else if (nbReq < threshold) {
+      if (triggered) {
+        triggered = false;
+        console.log('Yeah ! it recovered with ', nbReq, ' at ', new Date());
+      }
+    }
+  });
+}
 
+module.exports = bootstrap;
---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+require('./main');
+var fs = require('fs');
+
+describe('Trigger alert', function () {
+  it('should send an alert if more than 20  requests are sent in 2 minutes', function(done) {
+    done();
+  });
+});
